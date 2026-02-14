70e1c9ef04f2ad5c2e6c72f57e006f6cdc03823c
princejetro <adegbuyijephthah@gmail.com>
2026-02-14 04:17:05 +0100

    added static url

--- cbt_app/settings.py
+++ cbt_app/settings.py
@@ -136,7 +136,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+# STATIC_URL = 'static/'
+STATIC_URL = 'https://afgeuyozbkpjqrssgehc.supabase.co/storage/v1/object/public/main/staticfiles/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
